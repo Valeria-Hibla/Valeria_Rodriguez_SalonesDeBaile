46ef7b760e13424d012f8cdd21e1d94a1cab61a4
ValeriaHibla <valeriarodriguez1127989@correo.itm.edu.co>
2025-08-28 08:48:29 -0500

    Subete plis

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+public class Salones
+{
+
+    int CantidadSalon;
+    String? NombreSalon;
+    
+
+}
